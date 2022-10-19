d5681987321af247a6d84a935054fe0ee1593562
Jeff Fields <jkfields@yahoo.com>
2022-10-19 00:35:18 +0000

    committed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,9 +9,28 @@ def read_csv(fpath):
         data = DictReader(fh)
         return [ ln for ln in data ]
 
+
+def results():
+    output = """Election Results
+                -------------------------
+                Total Votes: 369711
+                -------------------------
+                Charles Casper Stockham: 23.049% (85213)
+                Diana DeGette: 73.812% (272892)
+                Raymon Anthony Doane: 3.139% (11606)
+                -------------------------
+                Winner: Diana DeGette
+                -------------------------"""
+
+    return "\n".join([ ln.strip() for ln in output.split("\n") ])
+
+
 def main():
     data = read_csv(fpath[0])
     print(data[-1])
+    print(f"Total ballots cast: {len(data)}")
+
+    print(results())
 
 
 if __name__ == "__main__":
