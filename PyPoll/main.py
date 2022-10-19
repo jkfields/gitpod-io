47c77aea184271d255ea44eae7db267a447029af
Jeff Fields <jkfields@yahoo.com>
2022-10-19 00:19:38 +0000

    committed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,12 @@
 import csv
 
+fpath = ( "/workspace/gitpod/PyPoll/Resources/election_data.csv",
+          "/workspace/gitpod/PyPoll/analysis/pypoll_analysis.txt"
+        )
+
+
 def main():
-    print("Hello, voters")
+    print(f"{fpath}")
 
 
 if __name__ == "__main__":
