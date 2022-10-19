e3dcf8e2d71c6c19d2f5a7fc5d11448de702b768
Jeff Fields <jkfields@yahoo.com>
2022-10-19 13:37:25 +0000

    commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,7 +27,7 @@ def average_change(data):
 
 # get the minimum change
 def get_maximum(data):
-    min_change = min(row.get("change") for row in data)
+    min_change = min(data, lambda row:row.get("change"))
     return min_change
 
 
