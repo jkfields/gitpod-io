86ba3347cf6af719713a180e1ab084c221b66f07
Jeff Fields <jkfields@yahoo.com>
2022-10-20 14:21:02 +0000

    2022-10-20 10:21:21

---
+++ PyPoll/jen2.py
@@ -0,0 +1,47 @@
+from csv import reader
+
+csvpath = "PyPoll/Resources/election_data.csv"
+
+num_rows = 0
+
+# candates name and number of votes received
+votes = {}
+
+with open(csvpath, encoding='utf-8') as csvfile:
+    csvreader = reader(csvfile, delimiter=',')
+    next(csvreader)
+
+    for row in csvreader:
+        num_rows += 1
+        candidate = row[2]
+        
+        if candidate in votes.keys():
+            votes[candidate] += 1
+        else:
+            votes[candidate] = 1
+
+results = []
+for candidate in votes.keys():
+    results.append(f"{candidate}: {round(100 * votes[candidate] / rows, 3)}% ({votes[candidate]})")
+
+winner = max(votes, key=votes.get)
+
+output = f"""
+  ```text
+  Election Results
+  ----------------------------
+  Total Votes: {rows}
+  ----------------------------
+  {row for row in results}
+  -------------------------
+  Winner: {winner}
+  -------------------------
+  ```
+
+"""
+
+# supposed to go in the analysis folder per assignment setup
+with ("analysis/pypoll.txt", "w") as fh:
+    fh.write(output)
+
+print(output)
