8a76d2b5207cc5590660cd60c6f990f579e8c610
Jeff Fields <jkfields@yahoo.com>
2022-10-24 18:12:53 +0000

    2022-10-2414:12:12

---
+++ PyRetire/app.py
@@ -0,0 +1,81 @@
+from datetime import date, datetime, timedelta
+
+holidays = [ date(2022, 10, 10),
+             date(2022, 11, 11),
+             date(2022, 11, 24),
+             date(2022, 11, 25),
+             date(2022, 12, 26),
+             date(2023, 1, 2),
+             date(2023, 1, 16),
+             date(2023, 2, 20),
+           ]
+time_off = [ date(2022, 9, 21),
+             date(2022, 9, 22),
+             date(2022, 9, 23),
+             date(2022, 9, 26),
+             date(2022, 11, 21),
+             date(2022, 11, 22),
+             date(2022, 11, 23),
+             date(2022, 11, 28),
+             date(2022, 12, 8),
+             date(2022, 12, 15),
+             date(2022, 12, 20),
+             date(2022, 12, 21),
+             date(2022, 12, 22),
+             date(2022, 12, 23),
+             date(2022, 12, 27),
+             date(2022, 12, 28),
+             date(2022, 12, 29),
+             date(2022, 12, 30),
+             date(2022, 12, 20),
+             date(2023, 3, 15),
+             date(2023, 3, 16),
+             date(2023, 3, 17),
+             date(2023, 3, 20),
+             date(2023, 3, 21),
+             date(2023, 3, 22),
+             date(2023, 3, 23),
+             date(2023, 3, 24),
+             date(2023, 3, 27),
+             date(2023, 3, 28),
+           ]
+
+
+def get_date():
+    while True:
+        dt = input("Retirement date (yyyy-mm-dd): ").strip()
+        try:
+            dt = datetime.strptime(dt, "%Y-%m-%d").date()
+
+        except ValueError as ex:
+            print(str(ex))
+
+        else:
+            return dt
+
+def number_of_business_days(start, end):
+    days = list_of_days(start, end)
+    return len(days)
+
+def list_of_days(start, end):
+    days = (start + timedelta(x + 1) for x in range((end - start).days))
+    return [ day.strftime("%Y-%m-%d")
+             for day in days
+               if day.weekday() < 5 and
+                  day not in holidays and
+                  day not in time_off
+            ]
+
+
+def main():
+    now = date.today()
+    retire = get_date()
+    #now = date(2022, 9, 23)
+    #retire = date(2023, 3, 28)
+
+    for day in list_of_days(now, retire):
+        print(day)
+    print(f"{number_of_business_days(now, retire)} remaining!")
+
+if __name__ == "__main__":
+    main()
