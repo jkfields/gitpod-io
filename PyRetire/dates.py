394510b5d57b7c9a67f0cf086f17de637249316d
Jeff Fields <jkfields@yahoo.com>
2022-10-29 23:58:43 +0000

    2022-10-29 19:58:37

---
+++ PyRetire/dates.py
@@ -0,0 +1,22 @@
+from dateutil import parser
+
+# initializing string
+test_str = '04-01-1997'
+
+# printing original string
+print("The original string is : " + str(test_str))
+
+# initializing format
+format = "%d-%m-%Y"
+
+# checking if format matches the date
+res = True
+
+# using try-except to check for truth value
+try:
+	res = bool(parser.parse(test_str))
+except ValueError:
+	res = False
+
+# printing result
+print("Does date match format? : " + str(res))
