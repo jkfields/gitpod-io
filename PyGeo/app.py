07e8410136322221a76617d3c942f5ead82439e8
Jeff Fields <jkfields@yahoo.com>
2022-11-06 00:28:25 +0000

    2022-11-05

--- PyGeo/app.py
+++ PyGeo/app.py
@@ -3,7 +3,7 @@ from geopy import geocoders
 def main():
     location = geocoders.Nominatim(user_agent="app")
     address = "404 Avondale Cir Warner Robins GA 31088"
-    address = "2545 VZ Country Rd 2120 Canton TX 75103"
+    address = "2545 VZ County Rd 2120 Canton TX 75103"
 
     data = location.geocode(address)
     print(data)
