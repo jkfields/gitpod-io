892e95155e290110c2f7925cc1465e9f1b0907b5
Jeff Fields <jkfields@yahoo.com>
2022-12-06 20:11:41 +0000

    2022-12-06

--- PyLdif/holidays.py
+++ PyLdif/holidays.py
@@ -39,12 +39,12 @@ def main():
     timeoff = get_timeoff()
     tcal = HolidayCalendarFactory('TimeOff', cal, timeoff)
     new_cal = tcal()
-    pp(new_cal.rules)
+    #pp(new_cal.rules)
     
     start = date.today()
     end = date(2023, 3, 24)
     work_days = CustomBusinessDay(calendar=new_cal)
-    #print(bdate_range(start, end, freq=work_days))
+    pp(bdate_range(start, end, freq=work_days))
 
 
 if __name__ == "__main__":
