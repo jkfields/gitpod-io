2793ba58f7228769473bc3dd8d561dd455cc7212
Jeff Fields <jkfields@yahoo.com>
2022-12-06 19:32:18 +0000

    2022-12-06

--- PyLdif/holidays.py
+++ PyLdif/holidays.py
@@ -1,5 +1,6 @@
-from pandas.tseries.holiday import get_calendar, HolidayCalendarFactory
+from pandas.tseries.holiday import get_calendar, HolidayCalendarFactory, GoodFriday
 from datetime import date
 
 cal = get_calendar("USFederalHolidayCalendar")
 print(cal)
+print(type(GoodFriday()))
